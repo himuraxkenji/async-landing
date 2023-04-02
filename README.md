60763afadf377c1406e71eedc5cd25bb3ab6f7a1
himuraxkenji <aaron.ariperto@gmail.com>
2023-04-02 13:49:49 -0300

    add readme description

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # async-landing
+
+The api used for this project is "The Metropolitan Museum of Art Collection API" 
+```
+https://metmuseum.github.io/
+```
--- src/assets/main.js
+++ src/assets/main.js
@@ -39,7 +39,7 @@ async function fetchData(urlApi){
         <div class="group relative">
         <div
           class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-          <img src="${picture.primaryImage}" alt="${picture.title}" class="max-w-sm h-auto">
+          <img src="${picture.primaryImage}" alt="${picture.title}" class="w-full">
         </div>
         <div class="mt-4 flex justify-between">
           <h3 class="text-sm text-gray-700">
